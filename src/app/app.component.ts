abdea547666540b0a9d76bd72c4bcb428220ce7f
@xouaib <gdasoft18@gmail.com>
2019-05-19 13:13:22 +0000

    Update Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularCrudPHP';
+  title = 'Angular 7 CRUD Demo Using PHP & MySQL';
 }
